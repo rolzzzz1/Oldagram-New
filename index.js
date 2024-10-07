f3f898843ab6b9a3ae6dc7bfe4f65adac8fb9d74
Aishwarya Sharma <roli.mradul@gmail.com>
2024-10-07 16:08:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,11 +89,8 @@ onValue(postsInDB, function (snapshot) {
 document.addEventListener("dblclick", function (e) {
   const targetId = e.target.id;
   const id = targetId.slice(-1);
-
-  // console.log(targetId.slice(-1));
-  // console.log(targetId.slice(0, -1));
-
   const target = targetId.slice(0, -1);
+
   if (target === "heartIcon") {
     const count = postsArray[id].likes + 1;
     const key = postsKeyArr[id];
@@ -114,116 +111,3 @@ document.addEventListener("dblclick", function (e) {
     update(idRef, updatedData);
   }
 });
-
-// onValue(postsInDB, function (snapshot) {
-//   let postsArray = Object.values(snapshot.val());
-//   let postsKeyArr = Object.keys(snapshot.val());
-//   // console.log(postsArray);
-
-//   mainPostContainer.innerHTML = "";
-
-//   for (let i = 0; i < postsArray.length; i++) {
-//     console.log(postsArray[i].name);
-
-//     let postContainer = document.createElement("div");
-//     postContainer.className = "post_container";
-//     postContainer.id = "post_container" + i;
-//     mainPostContainer.append(postContainer);
-
-//     let userInfoDiv = document.createElement("div");
-//     userInfoDiv.className = "user_info";
-//     postContainer.append(userInfoDiv);
-
-//     let avatarImg = document.createElement("img");
-//     avatarImg.src = postsArray[i].avatar;
-//     avatarImg.alt = "Avatar image";
-//     avatarImg.id = "avatar_img";
-//     avatarImg.className = "avatar";
-//     userInfoDiv.append(avatarImg);
-
-//     let userTextDiv = document.createElement("div");
-//     userTextDiv.className = "user_text";
-//     userInfoDiv.append(userTextDiv);
-
-//     let nameP = document.createElement("p");
-//     nameP.className = "bold_text";
-//     nameP.textContent = postsArray[i].name;
-//     userTextDiv.append(nameP);
-
-//     let locationP = document.createElement("p");
-//     locationP.textContent = postsArray[i].location;
-//     userTextDiv.append(locationP);
-
-//     let postImg = document.createElement("img");
-//     postImg.src = postsArray[i].post;
-//     postImg.id = "post_img" + i;
-//     postImg.className = "post_img";
-//     postImg.alt = "Post image";
-//     postContainer.append(postImg);
-
-//     let footerDiv = document.createElement("section");
-//     footerDiv.className = "footer_div";
-//     postContainer.append(footerDiv);
-
-//     let heartIcon = document.createElement("img");
-//     heartIcon.id = "heartIcon" + i;
-//     heartIcon.className = "icons";
-//     heartIcon.src = "./images/icon-heart.png";
-//     heartIcon.alt = "Heart icon";
-//     footerDiv.append(heartIcon);
-
-//     let commentIcon = document.createElement("img");
-//     commentIcon.className = "icons";
-//     commentIcon.src = "./images/icon-comment.png";
-//     commentIcon.alt = "Comment icon";
-//     footerDiv.append(commentIcon);
-
-//     let dmIcon = document.createElement("img");
-//     dmIcon.className = "icons";
-//     dmIcon.src = "./images/icon-dm.png";
-//     dmIcon.alt = "Dm icon";
-//     footerDiv.append(dmIcon);
-
-//     let likesP = document.createElement("p");
-//     likesP.textContent = postsArray[i].likes + " likes";
-//     likesP.className = "bold_text";
-//     footerDiv.append(likesP);
-
-//     let commentP = document.createElement("p");
-//     let username = document.createElement("span");
-//     username.className = "bold_text";
-//     username.textContent = postsArray[i].username;
-//     commentP.append(username);
-
-//     commentP.append(" " + postsArray[i].comment);
-//     footerDiv.append(commentP);
-
-//     let postImgDC = document.getElementById("post_img" + i);
-//     postImgDC.addEventListener("dblclick", function () {
-//       console.log("Doubled clicked post" + postImgDC.id);
-//       const count = postsArray[i].likes + 1;
-//       const key = postsKeyArr[i];
-
-//       let updatedData = {
-//         likes: count,
-//       };
-
-//       const idRef = ref(database, "oldagram/" + key);
-//       update(idRef, updatedData);
-//     });
-
-//     let heartIconDC = document.getElementById("heartIcon" + i);
-//     heartIconDC.addEventListener("dblclick", function () {
-//       console.log("Double clicked heart - " + heartIconDC.id);
-//       const count = postsArray[i].likes + 1;
-//       const key = postsKeyArr[i];
-
-//       let updatedData = {
-//         likes: count,
-//       };
-
-//       const idRef = ref(database, "oldagram/" + key);
-//       update(idRef, updatedData);
-//     });
-//   }
-// });
