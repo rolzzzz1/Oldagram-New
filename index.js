e6f90051a78069d814bea1ddb339c88def632b08
Aishwarya Sharma <roli.mradul@gmail.com>
2024-10-07 13:47:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ onValue(postsInDB, function (snapshot) {
     </div>
   `;
 
-    const postImgDC = document.getElementById(`post_img${i}`);
+    const imgId = "post_img" + i;
+    const postImgDC = document.getElementById(imgId);
     postImgDC.addEventListener("dblclick", function () {
       console.log("Doubled clicked post" + postImgDC.id);
       const count = postsArray[i].likes + 1;
